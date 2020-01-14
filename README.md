f4866c529b877d39730aaea6fd26ec35e9498ad8
jfqd <jfqd@blun.org>
2020-01-14 16:03:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# mi-qutic-limesurvey
+
+use [jfqd/mi-qutic.base](https://github.com/jfqd/mi-qutic-base) to create a provisionable image
+
+## description
+
+image with limesurvey, apache-24 and php71.
+
---
+++ copy/opt/qutic/var/mdata-setup/includes/54-limesurvey.sh
@@ -0,0 +1,16 @@
+if mdata-get server_name 1>/dev/null 2>&1; then
+  SERVER_NAME=`mdata-get server_name`
+  sed -i "s:SERVER_NAME:$SERVER_NAME:g" /opt/local/etc/httpd/vhosts/01-limesurvey.conf
+else
+  sed -i "s:ServerName SERVER_NAME::g" /opt/local/etc/httpd/vhosts/01-limesurvey.conf
+fi
+
+if mdata-get server_alias 1>/dev/null 2>&1; then
+  SERVER_ALIAS=`mdata-get server_alias`
+  sed -i "s:SERVER_ALIAS:$SERVER_ALIAS:g" /opt/local/etc/httpd/vhosts/01-limesurvey.conf
+else
+  sed -i "s:ServerAlias SERVER_ALIAS::g" /opt/local/etc/httpd/vhosts/01-limesurvey.conf
+fi
+
+# Enable apache by default
+/usr/sbin/svcadm enable svc:/pkgsrc/apache:default
